c93139ac61d57dfee4bac8dd8af2d11353f66fcb
Yash6012 <119106771+Yash6012@users.noreply.github.com>
2023-04-30 11:19:40 +0530

    Final

--- Assignment 6/4.py
+++ Assignment 6/4.py
@@ -1,27 +1,30 @@
 # Question 4
 # Creating function for binary search
 def b_search(my_list, key):
-    print(my_list)
-    low = 0
-    high = len(my_list) - 1
-    while low <= high:
-        mid = (low + high) // 2
+    initial = 0
+    last = len(my_list) - 1
+    while initial <= last:
+        mid = (initial + last) // 2
         if my_list[mid] == key:
             return mid
         elif my_list[mid] < key:
-            low = mid + 1
+            initial = mid + 1
         else:
-            high = mid - 1
+            last = mid - 1
     else:
         return -1
 
 
+# __main__
+# printing the initial information
+print("Program to search for index of an element using \"Binary Search\"")
 li = [1, 9, 7, 8, 6, 5, 4, 2, 3]
 li.sort()
-result = b_search(li, 0)
+print(li)
+# taking input from user whose index is to be searched
+x = int(input("Enter the number whose index you want to search in the above list: "))
+result = b_search(li, x)
 if result != -1:
-    print(f"Element is present at index {result}")
+    print("The element", x, "is present at ", result, "th index")
 else:
-    print("Element is not present in array")
-
-
+    print("The element", x, "is not present in the List")
