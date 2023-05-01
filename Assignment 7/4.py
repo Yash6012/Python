b37accaa7b348ebe815e008e4a7a4c2b7e7160d4
Yash6012 <119106771+Yash6012@users.noreply.github.com>
2023-05-01 23:39:23 +0530

    init2

--- Assignment 7/4.py
+++ Assignment 7/4.py
@@ -1,13 +1,16 @@
 # Question 4
-def invert_dict(d):
-    inverted = {}
-    for k, v in d.items():
-        inverted.setdefault(v, []).append(k)
+# Creating the function
+def invert_dict(dictionary):
+    inverted = {}  # Initializing a variable
+    # Parsing the dictionary
+    for k, v in dictionary.items():
+        inverted[v] = [k]  # adding Key:value pairs in dictionary
     return inverted
 
 
+# __main__
+# Initializing the dictionary
 my_dict = {"apple": 1, "banana": 2, "orange": 3, "pear": 4}
-# print(len(my_dict))
-# print(my_dict["apple"])
+# Calling the function
 inverted_dict = invert_dict(my_dict)
 print(inverted_dict)
