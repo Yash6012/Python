d32761eb8e7dc2c11f78c0563330fa38626aaa09
Yash6012 <119106771+Yash6012@users.noreply.github.com>
2023-05-01 23:33:45 +0530

    init2

--- Assignment 7/4.py
+++ Assignment 7/4.py
@@ -1,1 +1,13 @@
 # Question 4
+def invert_dict(d):
+    inverted = {}
+    for k, v in d.items():
+        inverted.setdefault(v, []).append(k)
+    return inverted
+
+
+my_dict = {"apple": 1, "banana": 2, "orange": 3, "pear": 4}
+# print(len(my_dict))
+# print(my_dict["apple"])
+inverted_dict = invert_dict(my_dict)
+print(inverted_dict)
