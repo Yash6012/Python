7a3c87a13c8615abb6f694e117d08c1623f7d1d5
Yash6012 <119106771+Yash6012@users.noreply.github.com>
2023-05-01 23:16:15 +0530

    Final

--- Assignment 7/3.py
+++ Assignment 7/3.py
@@ -1,1 +1,18 @@
 # Question 3
+def triangle(height):
+    i = 0
+    while i < height:
+        j = 0
+        while j <= i:
+            print("*", end="")
+            j += 1
+        print()
+        i += 1
+
+
+# __main__
+# Taking input of height from user
+x = int(input("Enter the height of triangle:"))
+print("The triangle made of asterisks of Height \"", x, "\" is")
+# Calling the function
+triangle(x)
