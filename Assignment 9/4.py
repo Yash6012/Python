90687080bdeab45da8c2890c0c426ca29b815ed3
Yash6012 <119106771+Yash6012@users.noreply.github.com>
2023-05-14 23:38:42 +0530

    Final

--- Assignment 9/4.py
+++ Assignment 9/4.py
@@ -1,9 +1,11 @@
 # Question 4
-from random import *
+
 # Taking input from user
 x = int(input("Enter the number: "))
+# Explicit typecasting to string
 y = str(x)
+# traversing the string in reverse order with -1 step
 if y == y[::-1]:
-    print("Your number {} is a Palindrome and you have won".format(x))
+    print("Your number {} is a \"Palindrome\" and you have won".format(x))
 else:
-    print("Your number {} is not a Palindrome and you have lost".format(x))
+    print("Your number {} is not a \"Palindrome\" and you have lost".format(x))
