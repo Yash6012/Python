9e15bd78e99c761c8eca42d971f75fb0d0732e51
Yash6012 <119106771+Yash6012@users.noreply.github.com>
2023-05-17 11:38:38 +0530

    Final

--- Assignment 8/6.py
+++ Assignment 8/6.py
@@ -1,11 +1,5 @@
-# Question 6
-# Initializing variable
-s = 0
-n = 1
-# Running till it finds a value
-while True:
-    s += n
-    n += 1
-    if s % 111 == 0:
-        print("The integers goes from 1, 2, 3, ....", n - 1, "and the sum obtained is:", s)
-        break
+import array as arr
+a = arr.array("d", [2, 3, 4, 5, 6, 7])
+print(a[5])
+print(a[-1])
+print(sort(a))
